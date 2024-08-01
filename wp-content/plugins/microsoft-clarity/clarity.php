b5b45d133323ee41f8afbbb69991765ccfabb79c
Prayatna Mishra <soyprayatna@Prayatnas-MacBook-Pro.local>
2024-08-01 22:03:34 +0545

    Paws and Vitality Section Added

---
+++ wp-content/plugins/microsoft-clarity/clarity.php
@@ -0,0 +1,5 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function plugin_dir_path() in /Users/soyprayatna/Local Sites/luminr/app/public/wp-content/plugins/microsoft-clarity/clarity.php:13
+Stack trace:
+#0 {main}
+  thrown in <b>/Users/soyprayatna/Local Sites/luminr/app/public/wp-content/plugins/microsoft-clarity/clarity.php</b> on line <b>13</b><br>
---
+++ wp-content/plugins/microsoft-clarity/clarity_page.php
@@ -0,0 +1,5 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function add_action() in /Users/soyprayatna/Local Sites/luminr/app/public/wp-content/plugins/microsoft-clarity/clarity_page.php:78
+Stack trace:
+#0 {main}
+  thrown in <b>/Users/soyprayatna/Local Sites/luminr/app/public/wp-content/plugins/microsoft-clarity/clarity_page.php</b> on line <b>78</b><br>
